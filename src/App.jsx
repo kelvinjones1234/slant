06fb254a93306f64165be8c632bc8544efdafccc
kelvinjones1234 <praisejournal2@gmail.com>
2024-06-29 21:30:03 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <div className="bg-bgColor">
         <Routes>
           {/* <Route path="/" element={<IntroPage />} /> */}
-          <Route path="/home-page" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
         </Routes>
