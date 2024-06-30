a16a993fcfeaa24d3d528b9863ebc55b9a896219
kelvinjones1234 <praisejournal2@gmail.com>
2024-06-30 19:29:37 +0100

    updated checkbox

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const RegisterPage = () => {
   return (
     <div className="bg-white flex justify-center h-screen max-w-[550px] mx-auto">
-      <div className="mt-[9vh]">
+      <div className="mt-[9vh] w-[90vw]">
         <h1 className="font-[500] text-[23px]">Create Account</h1>
         <p className="text-[14px] font-[400] opacity-70">
           Enter your name and password
@@ -14,7 +14,7 @@ const RegisterPage = () => {
           <div className="relative my-[8vh] border-b-[1px] border-primary">
             <input
               type="password"
-              className="text-[15px] font-[500] outline-none w-[70vw] max-w-[550px] form__input"
+              className="text-[15px] font-[500] outline-none form__input"
               required="required"
             />
             <span className="absolute left-0 opacity-50 pointer-events-none transition-transform duration-500">
@@ -24,7 +24,7 @@ const RegisterPage = () => {
           <div className="relative my-[8vh] border-b-[1px] border-primary">
             <input
               type="password"
-              className="text-[15px] font-[500] outline-none w-[70vw] max-w-[550px] form__input"
+              className="text-[15px] font-[500] outline-none max-w-[550px] form__input"
               required="required"
             />
             <span className="absolute left-0 opacity-50 pointer-events-none transition-transform duration-500">
@@ -34,18 +34,18 @@ const RegisterPage = () => {
           <div className="relative my-[8vh] border-b-[1px] border-primary">
             <input
               type="password"
-              className="text-[15px] font-[500] outline-none w-[70vw] max-w-[550px] form__input"
+              className="text-[15px] font-[500] outline-none max-w-[550px] form__input"
               required="required"
             />
             <span className="absolute left-0 opacity-50 pointer-events-none transition-transform duration-500">
               Password
             </span>
           </div>
-          <div className="flex gap-4 mt-[2.5vh] mb-[5vh] items-start w-[70vw] max-w-[550px] text-[14px]">
+          <div className="flex gap-4 mt-[2.5vh] mb-[5vh] items-start max-w-[550px] text-[14px]">
             <input type="checkbox" className="custom-checkbox mt-1" />
-            <p className="w-full">
-              I have read and agreed to Slant Menu{" "}
-              <span className="text-primary">Terms & Condition</span>
+            <p className="">
+              I have read and agreed to Slant Menu
+              <span className="text-primary"> Terms & Condition</span>
             </p>
           </div>
           <ButtonOne label="Sign Up" />
