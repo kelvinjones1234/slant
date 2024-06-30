c469f851fd45bed60127385e33f94e838b03869e
kelvinjones1234 <praisejournal2@gmail.com>
2024-06-30 19:32:07 +0100

    updated checkbox

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const LoginPage = () => {
   return (
     <div className="bg-white flex justify-center h-screen max-w-[550px] mx-auto">
-      <div className="mt-[9vh]">
+      <div className="mt-[9vh] w-[90vw]">
         <h1 className="font-[500] text-[23px]">Hello there 👋</h1>
         <p className="text-[14px] font-[400] opacity-70">
           Please enter your email and password to sign in
@@ -14,7 +14,7 @@ const LoginPage = () => {
           <div className="relative mt-[8vh] border-b-[1px] border-primary">
             <input
               type="text"
-              className="text-[15px] font-[500] outline-none w-[70vw] max-w-[550px] form__input"
+              className="text-[15px] font-[500] outline-none max-w-[550px] form__input"
               required="required"
             />
             <span className="absolute left-0 opacity-50 pointer-events-none transition-transform duration-500">
@@ -24,7 +24,7 @@ const LoginPage = () => {
           <div className="relative my-[8vh] border-b-[1px] border-primary">
             <input
               type="password"
-              className="text-[15px] font-[500] outline-none w-[70vw] max-w-[550px] form__input"
+              className="text-[15px] font-[500] outline-none max-w-[550px] form__input"
               required="required"
             />
             <span className="absolute left-0 opacity-50 pointer-events-none transition-transform duration-500">
