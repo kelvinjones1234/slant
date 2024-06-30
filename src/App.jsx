9561cdc7e18f7f055cf681921d1bd082ba47f624
kelvinjones1234 <praisejournal2@gmail.com>
2024-06-30 07:41:56 +0100

    fixed relative height

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,12 @@ import RegisterPage from "./pages/RegisterPage";
 function App() {
   return (
     <BrowserRouter>
-      <div className="bg-bgColor">
-        <Routes>
-          <Route path="/" element={<IntroPage />} />
-          <Route path="/home-page" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<IntroPage />} />
+        <Route path="/home-page" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
